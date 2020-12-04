490fc5c2125165bd86b6819bd8446488752116be
zzzhr <zzzhr@hotmail.com>
2020-12-04 21:26:47 +0800

    Auto commit

--- common/common_entity_grpc_pb.d.ts
+++ common/common_entity_grpc_pb.d.ts
@@ -1,1 +1,42 @@
-// GENERATED CODE -- NO SERVICES IN PROTO
+// package: services
+// file: common/common_entity.proto
+
+/* tslint:disable */
+/* eslint-disable */
+
+import * as grpc from "@grpc/grpc-js";
+import {handleClientStreamingCall} from "@grpc/grpc-js/build/src/server-call";
+import * as common_common_entity_pb from "../common/common_entity_pb";
+
+interface ICommonTestServiceService extends grpc.ServiceDefinition<grpc.UntypedServiceImplementation> {
+    testBoolEntity: ICommonTestServiceService_ITestBoolEntity;
+}
+
+interface ICommonTestServiceService_ITestBoolEntity extends grpc.MethodDefinition<common_common_entity_pb.BoolEntity, common_common_entity_pb.BoolEntity> {
+    path: "/services.CommonTestService/TestBoolEntity";
+    requestStream: false;
+    responseStream: false;
+    requestSerialize: grpc.serialize<common_common_entity_pb.BoolEntity>;
+    requestDeserialize: grpc.deserialize<common_common_entity_pb.BoolEntity>;
+    responseSerialize: grpc.serialize<common_common_entity_pb.BoolEntity>;
+    responseDeserialize: grpc.deserialize<common_common_entity_pb.BoolEntity>;
+}
+
+export const CommonTestServiceService: ICommonTestServiceService;
+
+export interface ICommonTestServiceServer {
+    testBoolEntity: grpc.handleUnaryCall<common_common_entity_pb.BoolEntity, common_common_entity_pb.BoolEntity>;
+}
+
+export interface ICommonTestServiceClient {
+    testBoolEntity(request: common_common_entity_pb.BoolEntity, callback: (error: grpc.ServiceError | null, response: common_common_entity_pb.BoolEntity) => void): grpc.ClientUnaryCall;
+    testBoolEntity(request: common_common_entity_pb.BoolEntity, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: common_common_entity_pb.BoolEntity) => void): grpc.ClientUnaryCall;
+    testBoolEntity(request: common_common_entity_pb.BoolEntity, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: common_common_entity_pb.BoolEntity) => void): grpc.ClientUnaryCall;
+}
+
+export class CommonTestServiceClient extends grpc.Client implements ICommonTestServiceClient {
+    constructor(address: string, credentials: grpc.ChannelCredentials, options?: Partial<grpc.ClientOptions>);
+    public testBoolEntity(request: common_common_entity_pb.BoolEntity, callback: (error: grpc.ServiceError | null, response: common_common_entity_pb.BoolEntity) => void): grpc.ClientUnaryCall;
+    public testBoolEntity(request: common_common_entity_pb.BoolEntity, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: common_common_entity_pb.BoolEntity) => void): grpc.ClientUnaryCall;
+    public testBoolEntity(request: common_common_entity_pb.BoolEntity, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: common_common_entity_pb.BoolEntity) => void): grpc.ClientUnaryCall;
+}
--- common/common_entity_grpc_pb.js
+++ common/common_entity_grpc_pb.js
@@ -1,1 +1,33 @@
-// GENERATED CODE -- NO SERVICES IN PROTO
+// GENERATED CODE -- DO NOT EDIT!
+
+'use strict';
+var grpc = require('@grpc/grpc-js');
+var common_common_entity_pb = require('../common/common_entity_pb.js');
+
+function serialize_services_BoolEntity(arg) {
+  if (!(arg instanceof common_common_entity_pb.BoolEntity)) {
+    throw new Error('Expected argument of type services.BoolEntity');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_services_BoolEntity(buffer_arg) {
+  return common_common_entity_pb.BoolEntity.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+
+var CommonTestServiceService = exports.CommonTestServiceService = {
+  testBoolEntity: {
+    path: '/services.CommonTestService/TestBoolEntity',
+    requestStream: false,
+    responseStream: false,
+    requestType: common_common_entity_pb.BoolEntity,
+    responseType: common_common_entity_pb.BoolEntity,
+    requestSerialize: serialize_services_BoolEntity,
+    requestDeserialize: deserialize_services_BoolEntity,
+    responseSerialize: serialize_services_BoolEntity,
+    responseDeserialize: deserialize_services_BoolEntity,
+  },
+};
+
+exports.CommonTestServiceClient = grpc.makeGenericClientConstructor(CommonTestServiceService);
